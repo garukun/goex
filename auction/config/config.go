6af1d259f59b6752c97fe6f6ffcb7239ed78c660
Steve Jiang <steve.jiang@vungle.com>
2015-09-15 20:50:24 -0700

    Refactor config for new config pattern.

---
+++ auction/config/config.go
@@ -0,0 +1,32 @@
+package auction
+import (
+	"time"
+	"github.com/garukun/goex/auction/types"
+	"flag"
+)
+
+const DEFAULT_AUCTION_TIMEOUT = 50 * time.Millisecond
+
+var timeout *time.Duration
+var auctionType *types.AuctionType
+
+func init() {
+	timeout = flag.Duration("auction_timeout", DEFAULT_AUCTION_TIMEOUT, "Duration for how long the auction should run.")
+	auctionType = flag.Uint("auction_type", 2, "Auction type, eg. first or second priced auction.")
+
+	validateFlags()
+}
+
+func validateFlags() {
+	if auctionType != uint(types.FIRST_PRICE) || auctionType != uint(types.SECOND_PRICE) {
+		panic("Usage: auction_type indicate either first priced auction (1) or second priced (2).")
+	}
+}
+
+func Tiemout() time.Duration {
+	return *timeout
+}
+
+func Type() types.AuctionType {
+	return types.AuctionType(*auctionType)
+}
